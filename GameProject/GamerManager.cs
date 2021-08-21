b70e84e9ade785dfbdd60f29f7d74f6541f39315
Baran Kurtbogan <barankurtbogan@gmail.com>
2021-08-21 19:05:34 +0300

    linq odevi 7.gun

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+    {
+        //bir sınıfın içinde başka sınıf kullanılacaksa ASLA ONU NEWLEME ! 
+        //onun yerine alttaki ctor oluşturulur
+
+        IUserValidationService userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            this.userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (userValidationService.Validate(gamer))
+            {
+                Console.WriteLine("Kayıt oldu.");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama başarısız!");
+            }
+            
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt silindi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt güncellendi");
+        }
+    }
+}
